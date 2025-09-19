ffe0067953275cc8275111df01939305da899a02
AYUSH RAJ <146729007+rajayush0@users.noreply.github.com>
2025-09-20 02:31:03 +0530

    Time: 7 ms (20.38%) | Memory: 11 MB (63.57%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -4,17 +4,23 @@ public:
         int n = s.size();
         map<char,int>freq;
         string ans = "";
+        //store each char by key value pair
         for(char c :s){
             freq[c]++;
         }
+        //as map is not sorted so we copy map in vector to sort 
         vector<pair<char,int>> vec(freq.begin(),freq.end());
         sort(vec.begin(),vec.end(),[](auto &a,auto &b){
             return a.second>b.second;
         });
-
-        for(auto it : vec){
-            ans.append(it.second,it.first);
-        }
+        
+        //
+            for(auto &it : vec) {
+                for(int i = 0; i < it.second; i++) {
+                    ans += it.first;  // repeat character
+                }
+            }
         return ans;
+        
     }
 };
