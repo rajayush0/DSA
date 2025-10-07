d7df9a184d54402737b2e95a1ae58737eb565dcd
AYUSH RAJ <146729007+rajayush0@users.noreply.github.com>
2025-10-08 01:10:59 +0530

    Time: 3 ms (18.47%) | Memory: 9 MB (54.08%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -3,24 +3,25 @@ public:
     string removeOuterParentheses(string s) {
         string ans = "";
         int n = s.size();
-        int start = 0;  // start of primitive
-        int end = 0;    // moving pointer
-        int open = 0;
+        int ptr1 = 0;   // start of primitive
+        int ptr2 = 0;   // moving pointer
+        int open = 0;   // balance counter
 
-        while (end < n) {
-            if (s[end] == '(') open++;
+        while (ptr2 < n) {
+            if (s[ptr2] == '(') open++;
             else open--;
 
-            // When open == 0, we found a complete primitive
+            // Primitive ends when open == 0
             if (open == 0) {
                 // Add inner part (exclude outermost '(' and ')')
-                ans += s.substr(start + 1, end - start - 1);
+                ans += s.substr(ptr1 + 1, ptr2 - ptr1 - 1);
 
-                // Move start to next primitive
-                start = end + 1;
+                // Move ptr1 to the start of next primitive
+                ptr1 = ptr2 + 1;
             }
 
-            end++;
+            // Move ptr2 forward
+            ptr2++;
         }
 
         return ans;
